dd4da22d587ec1be21de88596d5db899bf1cd7c6
wenfeng <florentxingwenfeng@gmail.com>
2026-05-12 12:29:53 -0400

    bug fix

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
 use std::fs;
 
-use serde_json::Map;
+use serde_json::{Map, Value};
 
 fn main()-> Result<(), Box<dyn std::error::Error>> {
     let file1 = fs::read_to_string("./testcases/test-1.json")?;
     let file2 = fs::read_to_string("./testcases/test-2.json")?;
 
-    let value1 = serde_json::from_str(&file1);
-    let value2 = serde_json::from_str(&file2);
+    let value1 = serde_json::from_str(&file1)?;
+    let value2 = serde_json::from_str(&file2)?;
 
     let mut map = Map::new();
 
-    if let Value::Object(value1) = json1 {
-        map.extend(value1);
+    if let Value::Object(value) = value1 {
+        map.extend(value);
     }
 
-    if let Value::Object(value2) = json2 {
-        map.extend(value2);
+    if let Value::Object(value) = value2 {
+        map.extend(value);
     }
 
     let result = Value::Object(map);
 
-    println!("{}", serde_json::to_string_pretty(result));
+    println!("{}", serde_json::to_string_pretty(&result)?);
 
     Ok(())
 }
